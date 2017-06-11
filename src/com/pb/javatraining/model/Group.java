a2027403dcf18c4826f6bc656c2c83187933febe
alexander <voloshyn.olexander@gmail.com>
2017-06-11 17:20:50 +0300

    new version

--- src/com/pb/javatraining/model/Group.java
+++ src/com/pb/javatraining/model/Group.java
@@ -1,13 +1,73 @@
 package com.pb.javatraining.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Group {
 
-    public String title;
-    public int groupNumber;
-    public Collection<Student> students;
+    private String title;
+    private int groupNumber;
+    private Collection<Student> students;
 
-    // TODO override equals
+    public Group(String title, int groupNumber) {
+        this.title = title;
+        this.groupNumber = groupNumber;
+        students = new ArrayList<>();
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(int groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+
+    public Collection<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Collection<Student> students) {
+        this.students = students;
+    }
+
+    public Integer averageMarkOfStudents() {
+        students.stream()
+                .mapToDouble(Student::averageMark)
+                .average()
+                .orElseThrow(() -> new RuntimeException("not averange marks"));
+
+        return (int) students
+                        .stream()
+                        .mapToDouble(Student::averageMark)
+                        .average().getAsDouble();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Group group = (Group) o;
+
+        if (groupNumber != group.groupNumber) return false;
+        if (!title.equals(group.title)) return false;
+        return students.equals(group.students);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + groupNumber;
+        result = 31 * result + students.hashCode();
+        return result;
+    }
 }
