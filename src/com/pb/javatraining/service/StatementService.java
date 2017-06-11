ad7d9dc162729e62e400d17038af611e7d08ae58
alexander <voloshyn.olexander@gmail.com>
2017-06-11 17:22:39 +0300

    new version

--- src/com/pb/javatraining/service/StatementService.java
+++ src/com/pb/javatraining/service/StatementService.java
@@ -1,80 +1,116 @@
 package com.pb.javatraining.service;
 
-import com.pb.javatraining.model.Branch;
-import com.pb.javatraining.model.Group;
-import com.pb.javatraining.model.Lesson;
-import com.pb.javatraining.model.Student;
+import com.pb.javatraining.model.*;
 
-import java.util.Collection;
-import java.util.Map;
-
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
-import static java.util.Collections.emptySet;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class StatementService {
 
     private final Collection<Branch> data;
+    private List<Group> groups;
 
     public StatementService() {
         DummyDataService dataService = new DummyDataService();
         this.data = dataService.getDummyData();
+
+        groups = data.stream()
+                .map(Branch::getGroups)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 
     public int getAmountOfGroupsWhereBadStudents() {
+        int countGroupsWithBadStudents = 0;
 
-        // TODO
-
-        return 0;
+        for (Group group : groups) {
+            if (group
+                    .getStudents()
+                    .stream()
+                    .filter(student -> student.averageMark() < 50)
+                    .count() > 0) {
+                countGroupsWithBadStudents++;
+            }
+        }
+        return countGroupsWithBadStudents;
     }
 
     public Map<Group, Integer> getAverageMarksWithinGroups() {
-
-        // TODO
-
-        return emptyMap();
+        return groups.stream().collect(Collectors.toMap(group -> group, Group::averageMarkOfStudents));
     }
 
     public Collection<String> getGroupTitlesWhereStudentsAreMen() {
-
-        // TODO
-
-        return emptyList();
+       return groups.stream()
+               .filter(group -> group.getStudents().stream().filter(student -> student.getState()== State.FEMALE).count() == 0)
+               .collect(Collectors.toList())
+               .stream()
+               .map(Group::getTitle)
+               .collect(Collectors.toList());
     }
 
     public Collection<String> getStudentFullNamesFromSpecifiedBranch(Branch branch) {
+        data.stream()
+                .filter(b-> b.getTitle().equals(branch.getTitle()))
+                .findAny()
+                .orElseThrow(()->new RuntimeException("branch is absent"));
 
-        // TODO
-
-        return emptyList();
+        return  data.stream()
+                .filter(b -> b.getTitle().equals(branch.getTitle()))
+                .map(Branch::getGroups)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList())
+                .stream()
+                .map(Group::getStudents)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList())
+                .stream()
+                .map(student -> student.getName() +" "+ student.getSurname())
+                .collect(Collectors.toList());
     }
 
     public Map<Lesson, Double> getAverageMarksWithinLessons() {
-
-        // TODO
-
-        return emptyMap();
+        // Collectors.toList & print
+       return groups.stream()
+                .map(Group::getStudents)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList()).stream()
+                .map(student -> student.getMarksByLessons().entrySet())
+                .flatMap(Collection::stream)
+                .collect(Collectors.groupingBy(Entry::getKey, Collectors.mapping(Map.Entry::getValue,Collectors.averagingDouble(p->p))));
     }
 
     public Collection<Student> getStudentsSuitableForArmy() {
-
-        // TODO
-
-        return emptyList();
+        return groups.stream()
+                .map(Group::getStudents)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList())
+                .stream()
+                .filter(student -> student.getAge() > 17 & student.getAge() <  21 & student.getState() == State.MALE)
+                .sorted(Comparator.comparing(Student::getAge))
+                .collect(Collectors.toList());
     }
 
     public Collection<Lesson> getAllLessons() {
-
-        // TODO
-
-        return emptySet();
+        return groups.stream()
+                .map(Group::getStudents)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList()).stream()
+                .map(student -> student.getMarksByLessons().entrySet())
+                .flatMap(Collection::stream)
+                .map(Entry::getKey).collect(Collectors.toList())
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public Collection<Group> getGroupsWhereAtLeastTwoExcelentStudents() {
+        groups.stream()
+                .filter(group -> group.getStudents().stream().filter(student -> student.averageMark() > 90).count() >= 2)
+                .findAny().orElseThrow(()-> new RuntimeException("Not groups with excellent students"));
+        return groups.stream()
+                .filter(group -> group.getStudents().stream().filter(student -> student.averageMark() > 90).count() >= 2)
+                .collect(Collectors.toList());
 
-        // TODO
-
-        return emptyList();
     }
-
 }
