27820b0c900661ec2405734d0c2608c50f61ca35
omenroman <omenroman1@gmail.com>
2017-06-13 13:31:25 +0300

    Finished homework

--- src/com/pb/javatraining/model/Branch.java
+++ src/com/pb/javatraining/model/Branch.java
@@ -1,12 +1,49 @@
 package com.pb.javatraining.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Branch {
 
-    public String title;
-    public Collection<Group> groups;
+    private String title;
+    private Collection<Group> groups;
 
-    // TODO override equals
+    public Branch(String title) {
+        this.title = title;
+        groups = new ArrayList<>();
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Collection<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Group group) {
+        groups.add(group);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Branch branch = (Branch) o;
+
+        if (!title.equals(branch.title)) return false;
+        return groups.equals(branch.groups);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + groups.hashCode();
+        return result;
+    }
 }
--- src/com/pb/javatraining/model/Lesson.java
+++ src/com/pb/javatraining/model/Lesson.java
@@ -2,9 +2,56 @@ package com.pb.javatraining.model;
 
 public class Lesson {
 
-    public String title;
-    public double credits;
+    private String title;
+    private double credits;
 
-    // TODO override equals
+    public Lesson(String title, double credits) {
+        this.title = title;
+        this.credits = credits;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        this.credits = credits;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Lesson lesson = (Lesson) o;
+
+        if (Double.compare(lesson.credits, credits) != 0) return false;
+        return title.equals(lesson.title);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = title.hashCode();
+        temp = Double.doubleToLongBits(credits);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson{" +
+                "title='" + title + '\'' +
+                ", credits=" + credits +
+                "}\n";
+    }
 }
