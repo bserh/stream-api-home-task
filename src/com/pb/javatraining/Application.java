e5bc4d78314caf476dcd86d8e339ab7fd383b85c
alexander <voloshyn.olexander@gmail.com>
2017-06-13 08:27:01 +0300

    added extended output

--- src/com/pb/javatraining/Application.java
+++ src/com/pb/javatraining/Application.java
@@ -29,36 +29,36 @@ public class Application {
         Map<Group, Integer> averageMarksWithinGroups = service.getAverageMarksWithinGroups();
         System.out.println("3. average marks within groups:");
         averageMarksWithinGroups
-                .forEach((k, v) -> System.out.println("\tgroup: " + k.getTitle() + " average mark: " + v));
+                .forEach((k, v) -> System.out.println("\tgroup: " + k.getTitle() + "\t|\taverage mark: " + v));
         System.out.println();
 
         Map<Lesson, Double> averageMarksWithinLessons = service.getAverageMarksWithinLessons();
         System.out.println("4. average marks within lessons:");
         averageMarksWithinLessons
-                .forEach((k, v) -> System.out.println("\tlesson: " + k.getTitle() + " average mark: " + v));
+                .forEach((k, v) -> System.out.format("%5s group: %20s \t| average mark: %.2f\n"," ",k.getTitle(), v.floatValue()));
         System.out.println();
 
-        Collection<Group> groupsWhereAtLeastTwoExcelentStudents = service.getGroupsWhereAtLeastTwoExcelentStudents();
+        Collection<Group> groupsWhereAtLeastTwoExcelentStudents = service.getGroupsWhereAtLeastTwoExcellentStudents();
         System.out.println("5. groups where at least two excelent students:");
         groupsWhereAtLeastTwoExcelentStudents
-                .forEach(group -> System.out.println(group.getTitle()));
+                .forEach(group -> System.out.println("\t" + group.getTitle()));
         System.out.println();
 
         Collection<String> groupTitlesWhereStudentsAreMen = service.getGroupTitlesWhereStudentsAreMen();
         System.out.println("6. group titles where students are men:");
         groupTitlesWhereStudentsAreMen
-                .forEach(System.out::println);
+                .forEach(s -> System.out.println("\t" + s));
         System.out.println();
 
         Branch branch = new Branch("information systems");
         Collection<String> studentFullNamesFromSpecifiedBranch = service.getStudentFullNamesFromSpecifiedBranch(branch);
         System.out.println("7. student full names from specified branch:");
         studentFullNamesFromSpecifiedBranch
-                .forEach(System.out::println);
+                .forEach(s -> System.out.println("\t" + s));
         System.out.println();
 
         Collection<Student> studentsSuitableForArmy = service.getStudentsSuitableForArmy();
-        System.out.println("8. students suitable for army:");
+        System.out.println("8. students suitable for army (from 18 to 20 ages):");
         studentsSuitableForArmy
                 .forEach(student -> System.out.println("\t" + student.getSurname() + " "
                         + student.getAge() + " "
