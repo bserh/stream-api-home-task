5e3ef1fac9cf3484a79c809bcbb34900697fec38
desking <dimakvashenko@mail.ru>
2017-06-13 09:34:36 +0600

    1

--- src/com/pb/javatraining/model/Branch.java
+++ src/com/pb/javatraining/model/Branch.java
@@ -1,12 +1,60 @@
 package com.pb.javatraining.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Branch {
 
-    public String title;
-    public Collection<Group> groups;
+    private String title;
+    private Collection<Group> groups;
+
+    public Branch(String title) {
+        this.title = title;
+        groups = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Collection<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Collection<Group> groups) {
+        this.groups = groups;
+    }
 
     // TODO override equals
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Branch branch = (Branch) o;
+
+        if (title != null ? !title.equals(branch.title) : branch.title != null) return false;
+        return groups != null ? groups.equals(branch.groups) : branch.groups == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (groups != null ? groups.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch{" +
+                "title='" + title + '\'' +
+                ", groups=" + groups +
+                '}';
+    }
 }
--- src/com/pb/javatraining/model/Student.java
+++ src/com/pb/javatraining/model/Student.java
@@ -1,15 +1,105 @@
 package com.pb.javatraining.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Student {
 
-    public String name;
-    public String surname;
-    public int age;
-    public State state;
-    public Map<Lesson, Double> marksByLessons;
+    private String name;
+    private String surname;
+    private int age;
+    private State state;
+    private Map<Lesson, Double> marksByLessons;
+
+
+    public Student(String name, String surname, int age, State state) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.state = state;
+        marksByLessons = new HashMap<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public Map<Lesson, Double> getMarksByLessons() {
+        return marksByLessons;
+    }
+
+    public void setMarksByLessons(Map<Lesson, Double> marksByLessons) {
+        this.marksByLessons = marksByLessons;
+    }
+
+    public void setMarksByLessons(Lesson lesson, Double mark) {
+        marksByLessons.put(lesson, mark);
+    }
 
     // TODO override equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Student student = (Student) o;
+
+        if (age != student.age) return false;
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        if (surname != null ? !surname.equals(student.surname) : student.surname != null) return false;
+        if (state != student.state) return false;
+        return marksByLessons != null ? marksByLessons.equals(student.marksByLessons) : student.marksByLessons == null;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", state=" + state +
+                ", marksByLessons=" + marksByLessons +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + age;
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        result = 31 * result + (marksByLessons != null ? marksByLessons.hashCode() : 0);
+        return result;
+
+
+    }
 }
